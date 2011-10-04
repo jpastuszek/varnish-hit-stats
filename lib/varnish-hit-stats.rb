d8ca603b452d34618cfc6719ce7adaf223372891
Jakub Pastuszek <jpastuszek@gmail.com>
2011-10-04 18:25:27 +0200

    moved code to to_csv

--- lib/varnish-hit-stats.rb
+++ lib/varnish-hit-stats.rb
@@ -22,10 +22,19 @@ class VarnishHitStats
 		end
 	end
 
-	def each
+	def each_page_class_stat
 		@stats.keys.sort.each do |page_class|
 			yield page_class, @stats[page_class]
 		end
+	end
+
+	def to_csv
+		out = ""
+		out += "class, pass, hit, miss, total, hit/total\n"
+		each_page_class_stat do |page_class, s|
+			out += "%s, %i, %i, %i, %i, %f\n" % [page_class, s.pass, s.hit, s.miss, s.total, s.hit_to_total_ratio]
+		end
+		out
 	end
 end
 
